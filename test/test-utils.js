3172792b77bf2f8efb1068b79d06859689d3b71f
Hamed Masafi <hamed.masafi@gmail.com>
2019-03-28 19:38:35 +0430

    missed files

---
+++ test/test-utils.js
@@ -0,0 +1,14 @@
+function fill_calendars(el) {
+    var select = document.getElementById(el)
+    for (var i = 0; i < __calendars_list.length; i++) {
+        var opt = document.createElement('option');
+        opt.value = __calendars_list[i];
+        opt.innerText = __calendars_list[i];
+        select.appendChild(opt);
+    }
+}
+
+function log(title, text) {
+    document.getElementById("p").innerHTML += "<strong>"
+        + title + ":</strong> " + text + "<br />";
+}
